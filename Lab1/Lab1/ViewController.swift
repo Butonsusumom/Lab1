a6bda69b5694fd8593da7ee8d3eda986ab24801d
Ksusha Tsubulko <43090851+Butonsusumom@users.noreply.github.com>
2021-04-07 09:06:31 -0700

    Add files via upload

---
+++ Lab1/Lab1/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Lab1
+//
+//  Created by Ksenia Tsybulko on 7.04.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var ss2: UILabel!
+    @IBOutlet weak var ss8: UILabel!
+    @IBOutlet weak var ss16: UILabel!
+    @IBOutlet weak var textEdit: UITextField!
+    var num = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+    @IBAction func buttonAction(_ sender: Any) {
+         num = Int(textEdit.text ?? "") ?? 0
+        ss2.text = String(num, radix: 2)
+        ss8.text = String(num, radix: 8)
+        ss16.text = String(num, radix: 16)
+    }
+    
+}
+
